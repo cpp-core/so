809a9e22033682ff0da6a019c8cddb7831ecc2bf
Mark Melton <mark.melton@notlem.com>
2023-04-29 14:06:30 -0400

    Update static_assert_v2 for negative case.

--- type-constraining-template-parameters-inside-concept/src/static_assert_v2.cpp
+++ type-constraining-template-parameters-inside-concept/src/static_assert_v2.cpp
@@ -13,8 +13,17 @@ consteval bool hash_func_impl(std::index_sequence<Is...>) {
     return (HasFunc<std::tuple_element_t<Is, T>> && ...);
 }
 
+template<class T, size_t... Is>
+consteval bool not_hash_func_impl(std::index_sequence<Is...>) {
+    static_assert((not HasFunc<std::tuple_element_t<Is, T>> || ...));
+    return (not HasFunc<std::tuple_element_t<Is, T>> || ...);
+}
+
 template<class T, size_t N = std::tuple_size_v<T>>
 concept HasFuncsN = hash_func_impl<T>(std::make_index_sequence<N>{});
+
+template<class T, size_t N = std::tuple_size_v<T>>
+concept NotHasFuncsN = not_hash_func_impl<T>(std::make_index_sequence<N>{});
 
 struct good { void func(); };
 struct bad { };
@@ -24,6 +33,7 @@ using nope = std::tuple<good, bad, good>;
 
 int main(int argc, const char *argv[]) {
     static_assert(HasFuncsN<ok>);
+    static_assert(NotHasFuncsN<nope>);
     static_assert(HasFuncsN<nope>);
     return 0;
 }
