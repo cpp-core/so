6a9df964819eb80e79ffd90bd3feb0336778dc15
Mark Melton <mark.melton@notlem.com>
2023-05-05 14:25:37 -0400

    Added s2 program.

--- scratch/src/s1.cpp
+++ scratch/src/s1.cpp
@@ -20,26 +20,26 @@ public:
     
     void ping() {
 	
-	while(counter <= 20) {
+	while(counter_ <= 20) {
 	    {
 		std::unique_lock<std::mutex> lck(mutex_);
 		cv0_.wait(lck, [this]{ return ready_ == false; });
 		ready_ = true;
-		std::cout << "ping counter: " << counter << std::endl;
+		std::cout << "ping counter: " << counter_ << std::endl;
 	    }
-	    ++counter;
+	    ++counter_;
 	    cv1_.notify_one();
 	}
     }
     
     void pong() {
 	
-	while(counter < 20) {  
+	while(counter_ < 20) {  
 	    {
 		std::unique_lock<std::mutex> lck(mutex_);
 		cv1_.wait(lck, [this]{ return ready_ == true; });
 		ready_ = false;
-		std::cout << "pong counter: " << counter << std::endl;
+		std::cout << "pong counter: " << counter_ << std::endl;
 	    }
 	    cv0_.notify_one();
 	}
@@ -49,10 +49,52 @@ private:
     bool ready_{false};
     std::mutex mutex_;
     std::condition_variable cv0_, cv1_; 
-    std::atomic<int> counter{};
+    std::atomic<int> counter_{};
+    std::thread t0_, t1_;
+};
+
+class PingPongAtomicFlag {
+public:
+    PingPongAtomicFlag() {
+	t0_ = std::thread([this]() { ping(); });
+	t1_ = std::thread([this]() { pong(); });
+    }
+
+    ~PingPongAtomicFlag() {
+	if (t0_.joinable())
+	    t0_.join();
+	if (t1_.joinable())
+	    t1_.join();
+    }
+    
+    void ping() {
+	
+	while(counter_ <= 20) {
+	    potato_.wait(true);
+	    std::cout << "ping counter: " << counter_ << std::endl;
+	    potato_.test_and_set();
+	    ++counter_;
+	    potato_.notify_one();
+	}
+    }
+    
+    void pong() {
+	
+	while(counter_ < 20) {
+	    potato_.wait(false);
+	    std::cout << "pong counter: " << counter_ << std::endl;
+	    potato_.clear();
+	    potato_.notify_one();
+	}
+    }
+
+private:
+    std::atomic_flag potato_;
+    std::atomic<int> counter_{};
     std::thread t0_, t1_;
 };
 
 int main(){
     PingPong p{};
+    // PingPongAtomicFlag paf{};
 }
---
+++ scratch/src/s2.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2022, 2023 by Mark Melton
+//
+
+#include "core/util/tool.h"
+
+int tool_main(int argc, const char *argv[]) {
+    return 0;
+}
