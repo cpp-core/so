580478b455a74ba9f390c26140847c766ddbd569
Mark Melton <mark.melton@notlem.com>
2023-05-07 00:10:30 -0400

    WIP.

--- scratch/src/s1.cpp
+++ scratch/src/s1.cpp
@@ -1,100 +1,37 @@
 
 #include <iostream>
-#include <condition_variable>
-#include <atomic>
-#include <thread>
+#include <vector>
 
-class PingPong {
-public:
-    PingPong() {
-	t0_ = std::thread(&PingPong::ping, this);
-	t1_ = std::thread(&PingPong::pong, this);
-    }
-
-    ~PingPong() {
-	if (t0_.joinable())
-	    t0_.join();
-	if (t1_.joinable())
-	    t1_.join();
-    }
-    
-    void ping() {
-	
-	while(counter_ <= 20) {
-	    {
-		std::unique_lock<std::mutex> lck(mutex_);
-		cv0_.wait(lck, [this]{ return ready_ == false; });
-		ready_ = true;
-		std::cout << "ping counter: " << counter_ << std::endl;
-	    }
-	    ++counter_;
-	    cv1_.notify_one();
-	}
-    }
-    
-    void pong() {
-	
-	while(counter_ < 20) {  
-	    {
-		std::unique_lock<std::mutex> lck(mutex_);
-		cv1_.wait(lck, [this]{ return ready_ == true; });
-		ready_ = false;
-		std::cout << "pong counter: " << counter_ << std::endl;
-	    }
-	    cv0_.notify_one();
-	}
-    }
-
+class Subject
+{
 private:
-    bool ready_{false};
-    std::mutex mutex_;
-    std::condition_variable cv0_, cv1_; 
-    std::atomic<int> counter_{};
-    std::thread t0_, t1_;
+    std::string name;
+    std::vector<int> sectionPercentages;
+
+public:
+    Subject()
+    {
+        std::cout << "Write the name of the subject: ";
+        std::cin >> this -> name;
+        int percentage;
+        
+        while(true){
+            std::cout << "Write a new section name and its percentage with no symbols: ";
+            std::cin >> percentage;
+            this -> sectionPercentages.push_back(percentage);
+        }
+    }
 };
 
-class PingPongAtomicFlag {
-public:
-    PingPongAtomicFlag() {
-	t0_ = std::thread([this]() { ping(); });
-	t1_ = std::thread([this]() { pong(); });
-    }
+int main()
+{
+    std::vector<Subject> subjects;
 
-    ~PingPongAtomicFlag() {
-	if (t0_.joinable())
-	    t0_.join();
-	if (t1_.joinable())
-	    t1_.join();
+    while(true)
+    {
+        Subject newSubject{};
+        subjects.push_back(newSubject);
     }
     
-    void ping() {
-	
-	while(counter_ <= 20) {
-	    potato_.wait(true);
-	    std::cout << "ping counter: " << counter_ << std::endl;
-	    potato_.test_and_set();
-	    ++counter_;
-	    potato_.notify_one();
-	}
-    }
-    
-    void pong() {
-	
-	while(counter_ < 20) {
-	    potato_.wait(false);
-	    std::cout << "pong counter: " << counter_ << std::endl;
-	    potato_.clear();
-	    potato_.notify_one();
-	}
-    }
-
-private:
-    std::atomic_flag potato_;
-    std::atomic<int> counter_{};
-    std::thread t0_, t1_;
-};
-
-int main(){
-    PingPong p{};
-    // PingPongAtomicFlag paf{};
+    return 0;
 }
