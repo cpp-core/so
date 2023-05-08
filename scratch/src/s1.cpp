42c14a38694feda8522ba992b63e1b75f1bbccbb
Mark Melton <mark.melton@notlem.com>
2023-05-07 20:30:17 -0400

    WIP/scratch

--- scratch/src/s1.cpp
+++ scratch/src/s1.cpp
@@ -1,37 +1,35 @@
 
 #include <iostream>
-#include <vector>
 
-class Subject
-{
-private:
-    std::string name;
-    std::vector<int> sectionPercentages;
-
-public:
-    Subject()
-    {
-        std::cout << "Write the name of the subject: ";
-        std::cin >> this -> name;
-        int percentage;
-        
-        while(true){
-            std::cout << "Write a new section name and its percentage with no symbols: ";
-            std::cin >> percentage;
-            this -> sectionPercentages.push_back(percentage);
-        }
+auto simple_count(const std::string& pattern, const std::string& str) {
+    size_t n{};
+    for (auto i = 0; i < str.size() - pattern.size() + 1; ++i) {
+	if (str.substr(i, pattern.size()) == pattern)
+	    ++n;
     }
-};
+    return n;
+}
 
-int main()
-{
-    std::vector<Subject> subjects;
-
-    while(true)
-    {
-        Subject newSubject{};
-        subjects.push_back(newSubject);
+auto find_count(const std::string& pattern, const std::string& str) {
+    size_t n{};
+    auto pos = str.find(pattern, 0);
+    while (pos != std::string::npos) {
+	++n;
+	pos = str.find(pattern, pos + 1);
     }
+    return n;
+}
+
+int main(int argc, const char *argv[]) {
+
+    // Read the pattern and string.
+    std::string pattern, str;
+    std::getline(std::cin, pattern);
+    std::getline(std::cin, str);
+
+    // Output the count.
+    std::cout << simple_count(pattern, str) << std::endl;
+    std::cout << find_count(pattern, str) << std::endl;
     
     return 0;
 }
