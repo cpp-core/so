caa006210eb0d24b72b18c0b9c23d0931629e68a
Mark Melton <mark.melton@notlem.com>
2023-05-11 07:08:59 -0400

    WIP/scratch

--- scratch/src/p4.cpp
+++ scratch/src/p4.cpp
@@ -1,27 +1,45 @@
 // Copyright (C) 2022, 2023 by Mark Melton
 //
 
-#include <cassert>
-#include <iostream>
+#include <concepts>
+#include <string>
+#include <tuple>
 
-int* scopeProblem()
-{
-    int *bret;
-    int b=10;
-    bret =&b;
-    
-    return bret;
-}
+template <typename t>
+concept type_model = requires(t p_t) {
+  typename t::inner;
+};
 
-auto foo() {
-    int a = 0, b = 1, c = 2;
-    return a + b + c;
-}
+template <typename t, typename u>
+concept same_inner = std::is_same_v<typename std::decay_t<t>::inner,
+				    typename std::decay_t<u>::inner>;
 
-int main(int argc, const char *argv[]) {
-    auto ptr = scopeProblem();
-    auto a = foo();
-    std::cout << *ptr << std::endl;
-    assert(a == 3);
-    return 0;
+template <typename t, typename... t_type_model>
+concept container_model = (same_inner<t, t_type_model> && ...);
+
+struct type_1 {
+  using inner = int;
+};
+
+struct type_2 {
+  using inner = std::string;
+};
+
+template <type_model... t_type_models> struct container {};
+
+template <type_model... t_type_model>
+void f(container_model<container<t_type_model...>> auto) {}
+
+using containers_X = container<type_1, type_2>;
+
+static_assert(not same_inner<type_1, type_2>);
+static_assert(not container_model<type_1, type_2>);
+
+int main() {
+
+  // containers_X _x;
+  // This produces a compilation error as expected.
+  // f(_x);
+
+  return 0;
 }
