7c3e12f8881eb7e0b8e239b52b7471d519cecb91
Mark Melton <mark.melton@notlem.com>
2023-05-17 13:42:44 -0400

    WIP/scratch.

---
+++ scratch/src/p0.py
@@ -0,0 +1,5 @@
+#!/bin/env python
+#
+
+with open("/dev/urandom") as fp:
+    fp.write("Hello, world!");
--- scratch/src/p1.cpp
+++ scratch/src/p1.cpp
@@ -1,34 +1,29 @@
 // Copyright (C) 2022, 2023 by Mark Melton
 //
 
-#include <atomic>
+#include <signal.h>
+#include <memory>
 #include <thread>
-#include <iostream>
+#include <unistd.h>
 
-class AtomicTest {
- public:
-  int AsyncTask() {
-    std::thread timer([this](){
-      while (not stop_.load(std::memory_order_acquire)) {
-        counter_.fetch_add(1, std::memory_order_relaxed);
-        std::this_thread::sleep_for(std::chrono::microseconds(10));
-      }
-      std::cout << "counter = " << counter_ << std::endl;
-    });
+void end_of_the_world(int) {
+    write(1, "Ouch\n", 5);
+}
     
-    std::this_thread::sleep_for(std::chrono::microseconds(100));
-    stop_.store(true, std::memory_order_release);
-    timer.join();
-    return 0;
-  }
-
- private:
-  std::atomic<int> counter_{0};
-  std::atomic<bool> stop_{false};
-};
-
 int main(int argc, const char *argv[]) {
-    AtomicTest test;
-    test.AsyncTask();
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = end_of_the_world;
+    sa.sa_flags = 0;
+    
+    sigaction(SIGSEGV, &sa, nullptr);
+    sigaction(SIGINT, &sa, nullptr);
+    sigaction(SIGTERM, &sa, nullptr);
+    sigaction(SIGQUIT, &sa, nullptr);
+
+    while (true)
+	std::this_thread::sleep_for(std::chrono::seconds{1});
     return 0;
 }
