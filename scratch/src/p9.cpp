c7511f14b54c6d966df54f020ce06cb12bac3da0
Mark Melton <mark.melton@notlem.com>
2023-05-26 17:12:13 -0400

    WIP/scratch

--- scratch/src/p9.cpp
+++ scratch/src/p9.cpp
@@ -2,52 +2,25 @@
 //
 
 #include <iostream>
-struct My1
-{
-    My1()
-    {
-        std::cout << "My1 constructor" << std::endl;
-    }
-    
-    ~My1()
-    {
-        std::cout << "My1 destructor" << std::endl;
-    }
-    
+
+using std::cout, std::endl;
+
+struct S0 {
+    char c;
+    int i;
 };
 
-void* thread1(void* pParam)
-{
-    std::shared_ptr<My1>* pp1 = (std::shared_ptr<My1>*)pParam;
-    
-    std::cout << "thread1 ref count: " << pp1->use_count() << std::endl;
-    
-    delete pp1;
-
-    return 0;
-}
-
-int main(int argc, const char * argv[])
-{
-    if(1)
-    {
-        std::shared_ptr<My1> p1 = std::make_shared<My1>();
-        
-        std::cout << "ref count: " << p1.use_count() << std::endl;
-        
-        std::shared_ptr<My1>* pp1 = new std::shared_ptr<My1>(p1);
-        pthread_t thr1;
-        if(pthread_create(&thr1, nullptr, thread1, (void*)pp1) != 0)
-        {
-            //Failed to start thread
-            delete pp1;
-        }
-        
-        std::cout << "ref count: " << p1.use_count() << std::endl;
-    }
-    
+#pragma pack(1)
+struct S1 {
+    char c;
     int i;
-    std::cin >> i;
+};
 
+int main(int argc, const char *argv[]) {
+    
+    cout << sizeof(S0) << endl;
+    cout << sizeof(S1) << endl;
+    
     return 0;
 }
+
